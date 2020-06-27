9e9d8dffb7ab7b4c1ec18b3b8ed6fc17380fe368
“Megan <meganschertz@gmail.com>
2020-06-26 23:08:42 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(int)
+    return false if int < 2
+    (2..int - 1).each do |x|
+        if (int % x) == 0
+            return false
+        end
+    end
+    true
+end
